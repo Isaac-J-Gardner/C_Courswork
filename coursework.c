8c2796c5d274b5fc28f682899a5382b76af92ff3
Isaac Gardner <isaacjamesgardner@hotmail.com>
2024-04-25 16:34:05 +0100

    Add files via upload

--- coursework.c
+++ coursework.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
-double map_prob(int position[2], char map[9][9], double *avg_steps, double *success_perc, double *std_dev){
-    srand(123456);
+double map_calc(int position[2], char map[9][9], double *avg_steps, double *success_perc, double *std_dev){
     int moves[8][2] = {{-1,0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}; //moves go clockwise from north round to north west
     int total_steps = 0;
     int successful_runs = 0.0;
-    int run_steps[1000]; //this will be used to calculate standard deviation
+    int run_steps[1000]; //this will be used to calculate standard deviation, 
+                         //since we cant know the mean until after all the runs, i have to store all individual runs
+                         //then use that to calculate S.D later
     for(int i = 0; i < 1000; i++){ //1000 random paths
         int current_position[2] = {position[0], position[1]}; //the start of the path
         for(int steps = 0; steps <= 10; steps++){ //10 steps, with the 11th loop for the case where it ran out of steps
@@ -23,72 +24,54 @@ double map_prob(int position[2], char map[9][9], double *avg_steps, double *succ
             int move_val = rand() % 8; //if the run has not ended, make the next random move
             int move[2] = {moves[move_val][0], moves[move_val][1]};
             int next_postion[2] = {current_position[0] + move[0], current_position[1] + move[1]};
-            if(!(next_postion[0] > 8) && !(next_postion[0] < 0) && !(next_postion[1] > 8) && !(next_postion[1] < 0)){
+            if(!(next_postion[0] > 8) && !(next_postion[0] < 0)){
                 current_position[0] = next_postion[0];
+            }
+            if(!(next_postion[1] > 8) && !(next_postion[1] < 0)){ //if the next move doesn't take the player off the map
                 current_position[1] = next_postion[1];
             }
-
         }
     }
-    if(successful_runs != 0){
+    if(successful_runs != 0){ //calculating average steps and standard deviation
         *avg_steps = total_steps/(successful_runs*1.0);
         double tot = 0.0;
-        for(int i = 0; i < successful_runs; i++){
-            double temp = run_steps[i] - *avg_steps;
-            temp *= temp;
-            tot += temp;
+        for(int i = 0; i < successful_runs; i++){ //for each successful run that was recorded
+            double temp = run_steps[i] - *avg_steps; //steps - mean
+            temp *= temp; //square the result
+            tot += temp; //sum of the squares
         }
-        tot = tot/(successful_runs);
-        *std_dev = sqrt(tot);
+        tot = tot/(successful_runs); //divide by n
+        *std_dev = sqrt(tot); //square root
     }
     else{
-        *avg_steps = 0;
+        *avg_steps = 0; //if successful runs was equal to 0, these will be as well
         *std_dev = 0;
     }
-    *success_perc = successful_runs/10.0;
+    *success_perc = successful_runs/10.0; //divide by 1000, times by 100
     return 0;
 }
 int main(){
+    srand(123456);
     char map[9][9]; //this will hold the 9x9 grid of characters read from the .txt file
     FILE *fptr; 
     fptr = fopen("island_map.txt", "r");
+    char temp;
     if(fptr != NULL){ //if the file could be found/opened
-        int j = 0; //used in the while loop, j is increased by 1 when a full line has successfully been added to the map
-        char *token; //used by strtok. it will point to the location in a string where a given character appears
-        char line[30]; //string that holds the line that has been read from the text file
-        while(fgets(line, 30, fptr) != NULL && j <= 9){ //if fgets is NULL, the end of the file has been reached. j should equal 8 when this while loop finishes
-            size_t line_length = strlen(line);
-            if(j < 8 && line_length != 18){ //if it isn't the last line, it should have a length of 20
-                printf("Error.");
-                return 1;
-            }
-            if(j == 8 && line_length != 17){ //the last line should have a length of 19 as it doesn't end with "\n"
-                printf("Error.");
-                return 1;
-            }
-            if(j > 8){ //if fgets() is not NULL and returns something when j is greater than 8, it has extra lines and so not correctly formatted
-                printf("Error.");
-                return 1;
-            }
-            token = strtok(line, " "); //token points to an array containing the 
-            int i = 0;
-            while (token != NULL && i < 9) {
-                if(strlen(token) != 1){ //we should only be getting 1 character, if there is more than 1, its not formatted correctly
-                    if(token[1] != '\n'){
-                        printf("Error.");
-                        return 1;
-                    }       
-                }
-                if(token[0] != 'B' && token[0] != 'W' && token[0] != 'L' && token[0] != 'D' && token[0] != 'V'){
-                    printf("Error.");
+        for (int row = 0; row < 9; row++){
+            for (int col = 0; col < 9; col++){
+                fscanf(fptr, "%c ", &temp);
+                if(temp != 'B' && temp != 'L' && temp != 'V' && temp != 'D' && temp != 'W'){
+                    printf("Error!");
                     return 1;
                 }
-                map[j][i] = token[0]; //add to map and get next token
-                token = strtok(NULL, " ");
-                i++;
+                map[row][col] = temp;
             }
-            j++;
         }
+        if(fscanf(fptr, "%c ", &temp) != EOF){
+            printf("Error!");
+            return 1;
+        }
+        
     }
     else{ //if fptr is NULL, the file could not be found/opened
         printf("unable to locate or open file");
@@ -114,12 +97,13 @@ int main(){
             position[0] = i;
             position[1] = j;
             double percentage, average_steps, stand_dev;
-            map_prob(position, map, &average_steps, &percentage, &stand_dev); //for each location in the map, calculate 1000 random walks
+            map_calc(position, map, &average_steps, &percentage, &stand_dev); //for each location in the map, calculate 1000 random walks
             perc[i][j] = percentage;
             steps_avg[i][j] = average_steps;
             standard_deviation[i][j] = stand_dev;
         }
     }
+    
     printf("Probability of escape:\n");
     for(int i = 0; i < 9; i++){ //displaying the percentage map
         for(int j = 0; j < 9; j++){
